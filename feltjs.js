bb2df50dc84083b83dfee65340c7c350bf9666a3
Alj <agnasaljhone@gmail.com>
2024-12-09 17:00:36 +0800

    Add files via upload

---
+++ feltjs.js
@@ -0,0 +1,13 @@
+// Optional script file to add interactivity if needed
+// Example: Smooth scrolling for anchor links
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
